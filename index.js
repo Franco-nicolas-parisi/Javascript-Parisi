3d076576b2b2bac505fe8df5c2aedebeb6fe1c9f
Franco Parisi <parisifrancoo@gmail.com>
2023-08-01 06:44:37 -0300

    arrays y demas

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 let nombreUsuario
-function solicitarNombre(){
+
+const solicitarNombre = () =>{
     do{nombreUsuario = prompt("Hola! Por favor, escribe tu nombre")
-}
-while(nombreUsuario == "" || nombreUsuario == " ")
+    }while(nombreUsuario == "" || nombreUsuario == " ")
 }
 
 solicitarNombre()
@@ -10,36 +10,88 @@ solicitarNombre()
 console.log("NOMBRE DE USUARIO: " + nombreUsuario)
 alert("Gracias por responder " + nombreUsuario + ".\n" + "\nHaga click en ACEPTAR para proceder a comprar")
 
-let cantidad
-let producto 
 
-do{producto = parseInt(prompt("Por favor, seleccione el producto que desea comprar. Cada uno incluirá el valor por unidad a su derecha" + "\n1)  Bolsa de chicles x100 unidades - $500" + "\n2)  Pipas Gigantes - $250 " + "\n3)  Bon o Bon - $150" + "\n4)  Chcocolate Blanco Cofler 200g - $400" + "\nPOR FAVOR, ESCRIBA EL NÚMERO INDICADO A LA IZQUIERDA DEL PRODUCTOR QUE DESEA ADQUIRIR"))
+const operacion = (op) =>{
+    if (op == "mult") {
+        return (a, b) => a * b
+    }
 }
-while(producto < 1 || producto > 4 || isNaN(producto)){
-    console.log("Ha seleccionado la opción " + producto)
-    switch (producto){
-        case 1:
-            do{cantidad = parseInt(prompt("Bolsa de chicles x100 unidades vale $500" + "\n¿Cuantas unidades desea?"))}
-            while(isNaN(cantidad) || cantidad == 0);
-            console.log("El total es: $" + (cantidad * 500))
-            break
-        case 2:
-            do{cantidad = parseInt(prompt("Pipas Gigantes vale $250" + "\n¿Cuantas unidades desea?"))}
-            while(isNaN(cantidad) || cantidad == 0);
-            console.log("El total es: $" + (cantidad * 250))
-            break
-        case 3:
-            do{cantidad = parseInt(prompt("Bon o Bon vale $150" +"\n¿Cuantas unidades desea?"))}
-            while(isNaN(cantidad) || cantidad == 0);
-            console.log("El total es: $" + (cantidad * 150))
-            break
-        case 4:
-            do{cantidad = parseInt(prompt("Chocolate Blanco Cofler 200g vale $400" +"\n¿Cuantas unidades desea?"))}
-            while(isNaN(cantidad) || cantidad == 0);
-            console.log("El total es: $" + (cantidad * 400))
-            break
-        default:
-            console.log("Vuelva a intentarlo")
-            break
-    }   
+
+class golosinas{
+    constructor(nombre,precio){
+        this.nombre = nombre,
+        this.precio = precio
+    }
 }
+
+const carrito = []
+
+const chicle = new golosinas("chicle", 10)
+const chocolate = new golosinas("chocolate", 100)
+const caramelo = new golosinas("caramelo", 5)
+const pipas = new golosinas("pipas", 200)
+
+let producto
+let cantidad 
+
+const comprar = () => {
+    do{
+        producto = parseInt(prompt("Que golosina desea comprar?" + "\n1) chicle $10" + "\n2) chocolate $100" + "\n3) caramelo $5" + "\n4) pipas $200" + "\nESCRIBA LA OPCIÓN NUMÉRICA"))
+    }while(producto != 1 && producto != 2 && producto != 3 && producto != 4){
+        switch(producto){
+            case 1:
+                do{cantidad = parseInt(prompt("¿Cuantas unidades desea? Escriba un número"))
+                }while(isNaN(cantidad) || (cantidad <= 0)){
+                    let total = operacion("mult")
+                    alert("Ha agregado " + cantidad + " unidad/es al carrito")
+                    console.log("El usuario agregó " + cantidad + " unidad/es de "+ chicle.nombre +" al carrito")
+                    carrito.push(total(cantidad, chicle.precio))};
+                    break;
+            case 2:
+                do{cantidad = parseInt(prompt("¿Cuantas unidades desea? Escriba un número"))
+                }while(isNaN(cantidad) || (cantidad <= 0)){
+                    let total = operacion("mult")
+                    alert("Ha agregado " + cantidad + " unidad/es al carrito")
+                    console.log("El usuario agregó " + cantidad + " unidad/es de "+ chocolate.nombre +" al carrito")
+                    carrito.push(total(cantidad, chocolate.precio))};
+                    break;
+            case 3:
+                do{cantidad = parseInt(prompt("¿Cuantas unidades desea? Escriba un número"))
+                }while(isNaN(cantidad) || (cantidad <= 0)){
+                    let total = operacion("mult")
+                    alert("Ha agregado " + cantidad + " unidad/es al carrito")
+                    console.log("El usuario agregó " + cantidad + " unidad/es de "+ caramelo.nombre +" al carrito")
+                    carrito.push(total(cantidad, caramelo.precio))};
+                    break;
+            case 4:
+                do{cantidad = parseInt(prompt("¿Cuantas unidades desea? Escriba un número"))
+                }while(isNaN(cantidad) || (cantidad <= 0)){
+                    let total = operacion("mult")
+                    alert("Ha agregado " + cantidad + " unidad/es al carrito")
+                    console.log("El usuario agregó " + cantidad + " unidad/es de "+ pipas.nombre +" al carrito")
+                    carrito.push(total(cantidad, pipas.precio))};
+                    break;   
+        }
+    }
+}
+    
+comprar ()
+
+let agregar 
+
+const agregarProducto = () => {do{ agregar = parseInt(prompt("Desea agregar otra golosina?" + "\n1) SI" + "\n2) NO" + "\nESCRIBA LA OPCIÓN NUMÉRICA"))}while(agregar != 1 && agregar != 2){
+    switch(agregar){
+        case 1: comprar();
+        case 2: break
+        }
+    }
+}
+
+agregarProducto()
+
+while(agregar === 1){agregarProducto()
+}
+
+const total = carrito.reduce((acumulador, elemento) => acumulador + elemento, 0)
+
+console.log("TOTAL A ABONAR: $" + total)
