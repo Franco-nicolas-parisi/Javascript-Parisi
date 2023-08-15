13433e511aa93be3e801a0e19f4c262ccf1c7d97
Franco Parisi <parisifrancoo@gmail.com>
2023-08-15 20:26:32 -0300

    preentrega3-edits

--- js/index.js
+++ js/index.js
@@ -1,97 +1,115 @@
-let nombreUsuario
+// HEADER
 
-const solicitarNombre = () =>{
-    do{nombreUsuario = prompt("Hola! Por favor, escribe tu nombre")
-    }while(nombreUsuario == "" || nombreUsuario == " ")
+let header = document.createElement("header")
+header.style.backgroundColor = "#ef6c9a"
+document.body.appendChild(header)
+
+
+let logo = document.createElement("div")
+logo.className = "logoynombre"
+logo.innerHTML = `<img src="../assets/logo.PNG"></img>`
+header.appendChild(logo)
+let logoNombre = document.createElement("a")
+logoNombre.className = "nombrepagina"
+logoNombre.innerHTML = `<a>BABAU</a>`
+logo.appendChild(logoNombre)
+
+let navbar = document.createElement("navbar")
+header.appendChild(navbar)
+navbar.className = "navbar"
+let ulnavbar = document.createElement("ul")
+ulnavbar.className = "ulnavbar"
+navbar.appendChild(ulnavbar)
+
+let itemsNavbar = [{nombre:"INICIO"},
+                    {nombre:"CONTACTO"},
+                    {nombre:"ABOUT"}]
+
+
+for(const item of itemsNavbar){
+    let navbarItem = document.createElement("li")
+    navbarItem.innerHTML = `<a href="../index.html">${item.nombre}</a>`
+    ulnavbar.appendChild(navbarItem)
 }
 
-solicitarNombre()
+// MAIN
 
-console.log("NOMBRE DE USUARIO: " + nombreUsuario)
-alert("Gracias por responder " + nombreUsuario + ".\n" + "\nHaga click en ACEPTAR para proceder a comprar")
+let main = document.createElement("main")
+document.body.appendChild(main)
+let cards = document.createElement("section")
+cards.className = "cards"
+main.appendChild(cards)
 
+const productos = [{id: 1, nombre:"Chicle", precio: 10, img:"../assets/chicle.webp"},
+                    {id: 2, nombre:"Chocolate", precio: 100, img:"../assets/chocolate.jpg"},
+                    {id: 3, nombre:"Caramelo", precio: 15, img:"../assets/caramelo.jpg"},
+                    {id: 4, nombre:"Pipas", precio: 200, img:"../assets/pipas.webp"}
+]
 
-const operacion = (op) =>{
-    if (op == "mult") {
-        return (a, b) => a * b
-    }
-}
+productos.forEach((producto) => {
+    let card = document.createElement("article")
+    card.className = "card"
+    card.innerHTML = `<img src="${producto.img}" class="card-img"></img>
+                        <div class="card-body">
+                            <h3 class="nombre-producto">${producto.nombre}</h3>
+                            <h4 class="valor-card">VALOR: $${producto.precio}</h4>
+                        </div>`;
+                        // <button class="btn-agregar">AGREGAR</button>  
+                        // <input type="text" class="input-cantidad" value=""></input>
+    cards.appendChild(card)
 
-class golosinas{
-    constructor(nombre,precio){
-        this.nombre = nombre,
-        this.precio = precio
-    }
-}
+    let agregar = document.createElement("button")
+    agregar.classList = "btn-agregar"
+    agregar.innerText = "AGREGAR"      
+    card.appendChild(agregar)
+
+    agregar.addEventListener("click", () =>{
+        carrito.push({
+            id: producto.id,
+            nombre: producto.nombre,
+            precio: producto.precio
+        })
+        localStorage.setItem("carritoStorage", JSON.stringify(carrito))
+    })   
+})
 
 const carrito = []
 
-const chicle = new golosinas("chicle", 10)
-const chocolate = new golosinas("chocolate", 100)
-const caramelo = new golosinas("caramelo", 5)
-const pipas = new golosinas("pipas", 200)
+let totalsection = document.createElement("section")
+totalsection.className = "total-section"
+main.appendChild(totalsection)
+let confirmarCompra = document.createElement("button")
+confirmarCompra.className = "btn-confirmar"
+confirmarCompra.innerText = "CONFIRMAR CARRITO"
+totalsection.appendChild(confirmarCompra)
+let totalproductos = document.createElement("div")
+totalproductos.className = "total-productos"
+totalsection.appendChild(totalproductos)
 
-let producto
-let cantidad 
+const carroconfirmado = 
 
-const comprar = () => {
-    do{
-        producto = parseInt(prompt("Que golosina desea comprar?" + "\n1) chicle $10" + "\n2) chocolate $100" + "\n3) caramelo $5" + "\n4) pipas $200" + "\nESCRIBA LA OPCIÓN NUMÉRICA"))
-    }while(producto != 1 && producto != 2 && producto != 3 && producto != 4){
-        switch(producto){
-            case 1:
-                do{cantidad = parseInt(prompt("¿Cuantas unidades desea? Escriba un número"))
-                }while(isNaN(cantidad) || (cantidad <= 0)){
-                    let total = operacion("mult")
-                    alert("Ha agregado " + cantidad + " unidad/es al carrito")
-                    console.log("El usuario agregó " + cantidad + " unidad/es de "+ chicle.nombre +" al carrito")
-                    carrito.push(total(cantidad, chicle.precio))};
-                    break;
-            case 2:
-                do{cantidad = parseInt(prompt("¿Cuantas unidades desea? Escriba un número"))
-                }while(isNaN(cantidad) || (cantidad <= 0)){
-                    let total = operacion("mult")
-                    alert("Ha agregado " + cantidad + " unidad/es al carrito")
-                    console.log("El usuario agregó " + cantidad + " unidad/es de "+ chocolate.nombre +" al carrito")
-                    carrito.push(total(cantidad, chocolate.precio))};
-                    break;
-            case 3:
-                do{cantidad = parseInt(prompt("¿Cuantas unidades desea? Escriba un número"))
-                }while(isNaN(cantidad) || (cantidad <= 0)){
-                    let total = operacion("mult")
-                    alert("Ha agregado " + cantidad + " unidad/es al carrito")
-                    console.log("El usuario agregó " + cantidad + " unidad/es de "+ caramelo.nombre +" al carrito")
-                    carrito.push(total(cantidad, caramelo.precio))};
-                    break;
-            case 4:
-                do{cantidad = parseInt(prompt("¿Cuantas unidades desea? Escriba un número"))
-                }while(isNaN(cantidad) || (cantidad <= 0)){
-                    let total = operacion("mult")
-                    alert("Ha agregado " + cantidad + " unidad/es al carrito")
-                    console.log("El usuario agregó " + cantidad + " unidad/es de "+ pipas.nombre +" al carrito")
-                    carrito.push(total(cantidad, pipas.precio))};
-                    break;   
-        }
-    }
+confirmarCompra.addEventListener("click", confirmacion = () => {const carroconfirmado = JSON.parse(localStorage.getItem("carritoStorage"));
+console.log(carroconfirmado)
+const total = carroconfirmado.reduce((acc,el)=>acc + el.precio, 0)
+console.log(total)
+totalproductos.innerHTML = `TOTAL A PAGAR: $${total}`
+})
+
+// FOOTER
+
+let iconsFooter = [{red: "wsp"},
+                    {red: "inst"},
+                    {red: "facebook"}
+]
+
+let footer = document.createElement("footer")
+document.body.appendChild(footer)
+let logosFooter = document.createElement("div")
+logosFooter.className = "logos-footer"
+footer.appendChild(logosFooter)
+
+for(const icon of iconsFooter){
+    let iconos = document.createElement("i")
+    iconos.innerHTML = `${icon.red}`
+    logosFooter.appendChild(iconos)
 }
-    
-comprar ()
-
-let agregar 
-
-const agregarProducto = () => {do{ agregar = parseInt(prompt("Desea agregar otra golosina?" + "\n1) SI" + "\n2) NO" + "\nESCRIBA LA OPCIÓN NUMÉRICA"))}while(agregar != 1 && agregar != 2){
-    switch(agregar){
-        case 1: comprar();
-        case 2: break
-        }
-    }
-}
-
-agregarProducto()
-
-while(agregar === 1){agregarProducto()
-}
-
-const total = carrito.reduce((acumulador, elemento) => acumulador + elemento, 0)
-
-console.log("TOTAL A ABONAR: $" + total)
